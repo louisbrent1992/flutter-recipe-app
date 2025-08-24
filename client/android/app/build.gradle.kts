e16d0e9341ec1423170926663ae5dd00136e913a
Louis Brent <louis_brent@Mac.lan>
2025-08-24 16:36:25 -0700

    Increment Android bundle version code to 10 for Google Play Console publishing

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         applicationId = "com.recipease.kitchen"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = flutter.versionName
     }
 
