a06f5bd94d6cbe12b9369089c8b09c3916827bab
Louis Brent <louis_brent@Mac.lan>
2025-11-20 09:03:39 -0800

    chore: bump version to 1.0.2+48
    - Update version code to 48
    - Rename config README.md to splash_screen.md

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "com.recipease.kitchen"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 30
+        versionCode = 48
         versionName = flutter.versionName
     }
 
