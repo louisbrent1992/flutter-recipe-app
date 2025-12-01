00400b8a50b435cfd982bc15e0118f07a09d6a82
Louis Brent <louis_brent@Mac.lan>
2025-12-01 07:34:03 -0800

    Update version and build configuration

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "com.recipease.kitchen"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 62
+        versionCode = 63
         versionName = flutter.versionName
     }
 
